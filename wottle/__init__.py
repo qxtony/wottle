d0783ec261dbafc03fef3d08f643f283cf2408a0
qxtony <71513188+qxtony@users.noreply.github.com>
2022-06-17 18:22:36 +0300

    Add files via upload
    Libraly files.

---
+++ wottle/__init__.py
@@ -0,0 +1,12 @@
+"""
+:authors: qxtony
+:license: MIT License
+
+:copyright (c) 2022 qxtony
+"""
+
+from wottle.api import Wottle
+
+__author__ = "qxtony"
+__version__ = "2.0.1"
+__email__ = "qxtonydev@gmail.com"
---
+++ wottle/api.py
@@ -0,0 +1,58 @@
+from typing import Dict, Optional, Union
+
+from aiohttp import ClientSession
+
+from wottle.exception import CityError, TokenError
+
+
+class Wottle:
+    def __init__(
+        self,
+        token: Optional[str] = None,
+        language: str = "ru"
+    ) -> None:
+
+        if not token:
+            raise TokenError(
+                "Invalid Token! You can get it here: "
+                "https://openweathermap.org/api."
+            )
+
+        self.token = token
+        self.language = language
+        self.url = "http://api.openweathermap.org/data/2.5/weather"
+
+        self.session = ClientSession(
+            headers={
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            }
+        )
+
+    async def get_weather(
+        self,
+        city: Optional[str] = None
+    ) -> Union[str]:
+
+        if not city:
+            raise CityError("Invalid city! In an empty city.")
+
+        response = await self.session.get(self.url, params=self.in_json(city))
+
+        json = await response.json()
+        await self.session.close()
+
+        if not json:
+            raise CityError(
+                "Invalid city! The city you specified was not found."
+            )
+
+        return json
+
+    def in_json(self, city: Union[str]) -> Dict[str, str]:
+
+        return {
+            "q": city,
+            "lang": self.language,
+            "appid": self.token
+        }
